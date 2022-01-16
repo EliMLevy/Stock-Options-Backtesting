a6d005942b963bd56f4d6cd44f7ffa666ff3ec57
Eli Levy <emlevy1@mail.yu.edu>
2022-01-16 13:31:19 -0500

    SPY overlay

--- V1/grapher.py
+++ V1/grapher.py
@@ -6,31 +6,56 @@ import datetime
 import pandas as pd
 
 # import output from sim_data
-from sim_data import output
 
 def make_graph(start_time, end_time, y_vals, output_name):
-    # x = np.array(pd.date_range(start=start_time,end=end_time))
-    x = [n for n in pd.date_range(start=start_time,end=end_time)]
-    # length = max(len(y_vals), len(x))
-    # plt.xticks([0, 1, 2],['2018','2019','2020'], rotation=20)
-    plt.figure(figsize=(10,5))
-    plt.xticks(rotation=40)
-    # plt.xaxis.set_minor_locator(MultipleLocator(5))
+    SPY_data = pd.read_csv("./SPY_Prices.csv")
+    relevant_SPY_data = SPY_data[(SPY_data["date"] >= str(start_time.date())) & (SPY_data["date"] <= str(end_time.date()))]
+    t = [n for n in pd.date_range(start=start_time,end=end_time)]
+
+    fig, ax1 = plt.subplots(figsize=(20, 5))
     plt.minorticks_on()
-    plt.plot(x, y_vals)
-    
-    plt.xlabel('Time')
-    plt.ylabel('Portfolio Value')
+
+    color = 'tab:red'
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('Portfolio Value', color=color)
+    ax1.plot(t, y_vals, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('SPY', color=color)  # we already handled the x-label with ax1
+    ax2.plot(t, relevant_SPY_data["price"], color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    # plt.show()
 
     plt.title(output_name)
-
     plt.savefig('./output/' + output_name + '.png')
-    plt.clf()
+
+
+
+    # # Set up figure
+    # plt.xticks(rotation=40)
+    # plt.xlabel('Time')
+    # plt.ylabel('Portfolio Value')
+    # # Plot portfolio
+    # x = [n for n in pd.date_range(start=start_time,end=end_time)]
+    # plt.plot(x, y_vals, label="Portfolio")
+    # # Plot SPY
+    # SPY_data = pd.read_csv("./SPY_Prices.csv")
+    # relevant_SPY_data = SPY_data[(SPY_data["date"] >= str(start_time.date())) & (SPY_data["date"] <= str(end_time.date()))]
+    # plt.plot(x, relevant_SPY_data["price"], label="SPY")
+    
+    # plt.legend()
+    # plt.savefig('./output/' + output_name + '.png')
+    # plt.clf()
 
 
 def main():
-    start = datetime.datetime(2006, 1, 1)
-    end =  datetime.datetime(2021, 8, 31)
+    start = datetime.datetime(2005, 1, 10)
+    end =  datetime.datetime(2021, 9, 2)
 
     make_graph(start, end, output.y_vals, "graph")
 
